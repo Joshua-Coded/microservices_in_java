5e58359d2a316cc72ac79524b278f46c5abed935
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-12-20 01:33:31 +0200

    the first service is initialized

---
+++ product-service/src/test/java/com/alana/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alana.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
