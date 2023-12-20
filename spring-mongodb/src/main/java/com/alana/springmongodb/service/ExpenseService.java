2121b94fdf5e11de80499bb3214cdb195965df3f
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-12-21 01:27:28 +0200

    a new spring-mongodb spring boot project

---
+++ spring-mongodb/src/main/java/com/alana/springmongodb/service/ExpenseService.java
@@ -0,0 +1,13 @@
+package com.alana.springmongodb.service;
+
+public class ExpenseService {
+
+    public void addExpense(){}
+    public void updateExpense(){}
+
+    public  void getAllExpenses(){}
+
+    public void getExpenseByName(){}
+
+    public void deleteExpense(){}
+}
