b93a1c2c0c79e37b99c942667e9d169ae6d3b6fc
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-12-21 00:51:59 +0200

    a new spring-mongodb spring boot project

---
+++ spring-mongodb/src/test/java/com/alana/springmongodb/SpringMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alana.springmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spring-mongodb/src/test/java/com/alana/springmongodb/TestSpringMongodbApplication.java
@@ -0,0 +1,23 @@
+package com.alana.springmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestSpringMongodbApplication {
+
+	@Bean
+	@ServiceConnection
+	MongoDBContainer mongoDbContainer() {
+		return new MongoDBContainer(DockerImageName.parse("mongo:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(SpringMongodbApplication::main).with(TestSpringMongodbApplication.class).run(args);
+	}
+
+}
