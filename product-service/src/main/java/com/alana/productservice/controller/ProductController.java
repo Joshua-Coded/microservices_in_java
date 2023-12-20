97447a9392e14b18b761756d7439c86d3bc39a32
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-12-20 21:59:11 +0200

    created a dto package where i have my productRequest class

---
+++ product-service/src/main/java/com/alana/productservice/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.alana.productservice.controller;
+
+import com.alana.productservice.dto.ProductRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+ @RequestMapping("/api/product")
+public class ProductController {
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+
+    }
+}
